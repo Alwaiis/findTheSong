e78c5e5850a53176f5f89c75505ecafe3fbdb206
Naiara <garcianaiara04@gmail.com>
2024-05-22 19:59:28 +0200

    extracting italic words, done

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from PyPDF2 import PdfReader
+import re
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTChar, LTTextBox
 
 
 # ->[str] means that the def must return an array of strings
 def extract_text_from_pdf(pdf_file: str)-> [str]:
-    with open(pdf_file, 'rb') as pdf:
-        reader = PdfReader(pdf, strict=False)
-        pdf_text = []
-        for page in reader.pages:
-            content = page.extract_text()
-            pdf_text.append(content)
+        italic_text = []
 
-        return pdf_text
+        for page_layout in extract_pages(pdf_file):
+            for element in page_layout:
+                if isinstance(element, LTTextBox):
+                    for text_line in element:
+                        word = ""
+                        for character in text_line:
+                            if isinstance(character, LTChar):
+                                if 'Italic' in character.fontname:
+                                    word += character.get_text()
+                        if word:
+                            italic_text.append(word)
+
+        return italic_text
 
 
 if __name__ == '__main__':
