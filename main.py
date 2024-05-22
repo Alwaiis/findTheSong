71914a9754d1b6bd918b54371594c0c964a09b48
Naiara <garcianaiara04@gmail.com>
2024-05-22 21:19:35 +0200

    checking if exists in deezer, done

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import re
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTChar, LTTextBox
+from Song_List import SongList
+import requests
 
-
+def song_exist_on_deezer(song: str) -> bool:
+    response = requests.get(f'https://api.deezer.com/search?q={song}')
+    data = response.json()
+    if 'data' in data:
+        return len(data['data']) > 0
+    else:
+        print(" ")
+        return False
 # ->[str] means that the def must return an array of strings
 def extract_text_from_pdf(pdf_file: str)-> [str]:
         italic_text = []
@@ -16,7 +25,7 @@ def extract_text_from_pdf(pdf_file: str)-> [str]:
                             if isinstance(character, LTChar):
                                 if 'Italic' in character.fontname:
                                     word += character.get_text()
-                        if word:
+                        if word and len(word.strip()) > 1:
                             italic_text.append(word)
 
         return italic_text
@@ -25,4 +34,5 @@ def extract_text_from_pdf(pdf_file: str)-> [str]:
 if __name__ == '__main__':
     extracted_text = extract_text_from_pdf('TLQNF.pdf')
     for text in extracted_text:
-        print(text)
+        if song_exist_on_deezer(text):
+            print(text)
